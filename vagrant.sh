6728fb4ea0c3f0624c4ef4d50d77ac9090d648d8
mxmo0rhuhn <code@schrimpf.ch>
2015-06-17 01:56:29 +0200

    Vagrantfile for Gentoo with Kernel 3.10.77 and 4.0.5

--- vagrant.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-vagrant box add Gentoo_amd64_minimal https://dl.dropboxusercontent.com/s/mfurnvstqoj8w47/gentoo-20131024-amd64-minimal.box
-vagrant init Gentoo_amd64_minimal
-vagrant up
